206d50c708d04ee6c722a05f9abfdb50c987fda3
sergey <seraphicps@mail.ru>
2017-04-04 23:37:49 +0300

    1

--- src/com/netcracker/edu/inventory/service/impl/RackServiceImpl.java
+++ src/com/netcracker/edu/inventory/service/impl/RackServiceImpl.java
@@ -2,27 +2,36 @@ package com.netcracker.edu.inventory.service.impl;
 
 import com.netcracker.edu.inventory.model.Rack;
 import com.netcracker.edu.inventory.service.RackService;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class RackServiceImpl implements RackService {
-    @Override
+
+    static protected Logger LOGGER = Logger.getLogger(RackServiceImpl.class.getName());
+
     public void outputRack(Rack rack, OutputStream outputStream) throws IOException {
 
     }
 
-    @Override
+
     public Rack inputRack(InputStream inputStream) throws IOException, ClassNotFoundException {
         return null;
     }
 
-    @Override
-    public void writeRack(Rack rack, Writer writer) throws IOException {
 
+    public void writeRack(Rack rack, Writer writer) throws IOException {
+        NotImplementedException notImplementedException = new NotImplementedException();
+        LOGGER.log(Level.SEVERE, notImplementedException + ", An unfinished execution path");
+        throw notImplementedException;
     }
 
-    @Override
+
     public Rack readRack(Reader reader) throws IOException, ClassNotFoundException {
-        return null;
+        NotImplementedException notImplementedException = new NotImplementedException();
+        LOGGER.log(Level.SEVERE, notImplementedException + ", An unfinished execution path");
+        throw notImplementedException;
     }
 }
